a7dc3858742dbf7ee6e87549fcb18e4c1b9836c1
NavodContrath <navod.contrath@gmail.com>
2025-03-31 13:03:15 +0200

    Add form for submitting post details in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,35 @@ function App() {
 
   return (
     <>
+
+      <div className="container my-5" >
+        <div className="card p-5" >
+          <div className="card-head">
+            <h3>Send your request</h3>
+          </div>
+          <div className="card-body">
+
+            <form className=" g-3 needs-validation" novalidate>
+              <div>
+                <label for="validationCustom01" className="form-label">Post's author</label>
+                <input type="text" className="form-control" id="author-name" name="author-name" required />
+              </div>
+              <div >
+                <label for="validationCustom02" className="form-label">Post's title</label>
+                <input type="text" className="form-control" id="post-title" name="post-title" required />
+              </div>
+
+              <div className="mb-3">
+                <label for="" className="form-label">Post's content</label>
+                <textarea className="form-control" name="post-content" id="post-content" rows="3"></textarea>
+              </div>
+              <div>
+                <button className="btn btn-primary" type="submit">Submit form</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
